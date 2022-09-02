ad8a65ece3b27a02be30a64e2ec3b7e730464ba3
João Victor Rodrigues <jvpr123@outlook.com>
2022-09-02 12:06:33 -0300

    feat: :sparkles: add OpenZeppelin Crodsale.sol to project and inherit it in AlphaTokenCrowdsale contract
    update contract from v2.5.0 to work according compiler version (^0.8.15)

---
+++ contracts/crowdsale/AlphaTokenCrowdsale.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "./@openzeppelin/Crowdsale.sol";
+
+contract AlphaTokenCrowdsale is Crowdsale {
+    constructor(
+        uint256 _rate,
+        address payable _wallet,
+        IERC20 _token
+    ) Crowdsale(_rate, _wallet, _token) {}
+}
